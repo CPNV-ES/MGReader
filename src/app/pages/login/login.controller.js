62aa374e90b75c675fd3699ef5c1916bc20b15cc
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-03-07 09:27:55 +0100

    feat: test to log in a person

--- src/app/pages/login/login.controller.js
+++ src/app/pages/login/login.controller.js
@@ -1,5 +1,12 @@
-function checkLoginStatus(User) {
-    return User.status;
-}
+export default {
+    // Return the status of the mockResponse
+    checkLoginStatus: function(User) {
+        return User.status;
+    },
 
-module.exports = { checkLoginStatus };
+    // Log a person into our app after Facebook has authenticated him
+    logPersonIn: function(User) {
+        // Logic to create a session with the user
+        return 'logged';
+    },
+
--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -24,3 +24,25 @@ import login from "../app/pages/login/login.controller";
 
     });
 
+    test('Log_PersonIn_SuccessfullyLogged', () => {
+        //Given
+        // Login response from Facebook mocked
+        const mockResponse = {
+            status: 'connected',
+            authResponse: {
+                accessToken: '{access-token}',
+                expiresIn: '{unix-timestamp}',
+                reauthorize_required_in: '{seconds-until-token-expires}',
+                signedRequest: '{signed-parameter}',
+                userID: '{user-id}'
+            }
+        };
+
+        //When
+        // Log person in using the mocked response
+        jest.spyOn(login, 'logPersonIn').mockReturnValue('logged');
+
+        //Then
+        // Expect the login status to be logged
+        expect(login.logPersonIn(mockResponse)).toEqual('logged');
+    });
